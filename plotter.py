0a699a52f2967620b1ac6293e128a85f5a881cef
WhiteDragon251 <58987388+WhiteDragon251@users.noreply.github.com>
2021-10-05 21:03:48 +0530

    Update plotter.py
    Removed unnecessary comments
    Added code to make the plotting a little better

--- plotter.py
+++ plotter.py
@@ -8,65 +8,43 @@ class Date:
         self.y = y
  
  
-# To store number of days in all months from
-# January to Dec.
 monthDays = [31, 28, 31, 30, 31, 30,
              31, 31, 30, 31, 30, 31]
- 
-# This function counts number of leap years
-# before the given date
  
  
 def countLeapYears(d):
  
     years = d.y
  
-    # Check if the current year needs to be considered
-    # for the count of leap years or not
     if (d.m <= 2):
         years -= 1
  
-    # An year is a leap year if it is a multiple of 4,
-    # multiple of 400 and not a multiple of 100.
     return int(years / 4) - int(years / 100) + int(years / 400)
  
- 
-# This function returns number of days between two
-# given dates
+
 def getDifference(dt1, dt2):
- 
-    # COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
- 
+
     # initialize count using years and day
     n1 = dt1.y * 365 + dt1.d
  
     # Add days for months in given date
     for i in range(0, dt1.m - 1):
         n1 += monthDays[i]
- 
-    # Since every leap year is of 366 days,
-    # Add a day for every leap year
+
     n1 += countLeapYears(dt1)
- 
-    # SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2'
  
     n2 = dt2.y * 365 + dt2.d
     for i in range(0, dt2.m - 1):
         n2 += monthDays[i]
     n2 += countLeapYears(dt2)
- 
-    # return difference between two counts
+    
     return (n2 - n1)
  
-
 
 current_date = [ int(x) for x in str(date.today()).split('-') ]
  
 ref_date = Date(1, 1, 2000)
 current_date = Date(current_date[2], current_date[1], current_date[0])
-# test = Date(30, 8, 1956)
- 
-# print("Today is:", current_date)
 
 D_difference_of_days = getDifference(ref_date, current_date)
 
@@ -133,6 +111,7 @@ def planet_orbit(distance_from_sun, radius_of_planet_around_sun):
 
 #plotting
 def plot_planet(distance_from_sun, radius_of_planet_around_sun, planet_color, adjustment, reading, radius_of_planet):  
+  tr.setheading(0)
   tr.speed()
   tr.penup()
   tr.color('white')
@@ -152,6 +131,7 @@ def plot_planet(distance_from_sun, radius_of_planet_around_sun, planet_color, ad
 
   tr.color(planet_color)
   tr.begin_fill()
+  tr.setheading(0)
   tr.circle(radius_of_planet)
   tr.end_fill()
     
@@ -169,9 +149,6 @@ plot_planet(35.98, 150, 'red', 0.54, mercury, 1.516+2)
 # earth
 tr.setheading(0)
 plot_planet(92.96, 430, 'blue', 0.97, 1-earth, 3.958+2)
-
-# print("Mercury", mercury)
-# print("Earth", earth)
 
 tr.hideturtle()
 
